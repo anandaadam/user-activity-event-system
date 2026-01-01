b6ed123838f347426189071d220c9e80ba44a248
Adam Ananda Santoso <adamananda02@gmail.com>
2026-01-01 16:17:06 +0700

    Initial Commit

---
+++ src/test/java/user_activity_event_system/core/UserActivityEventSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package user_activity_event_system.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserActivityEventSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
